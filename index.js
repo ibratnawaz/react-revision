7eb28788effdbc7d5732de88deef9917936a8996
Ibrat Nawaz <nawazmd0096@gmail.com>
2023-07-09 15:55:34 +0530

    feat: react using npm package

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-// Using Old ways to create react element & rendering on page.
-const heading = React.createElement('div', { id: 'parent' }, [
-	'Section Heading',
-	React.createElement('h1', { id: 'heading' }, 'Hello React')
-]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+import React from 'react';
+import ReactDom from 'react-dom/client';
+
+// React element using JSX
+const appTitle = <h1>Hello React from JSX 👋</h1>;
+
+function ChildComponent () {
+  return <h1>I'm a child component 😁</h1>
+}
+
+
+// React functional component
+const HeadingComponent = () => {
+	return (
+    <div>
+      {appTitle}
+      Empty tag way <br/>
+      <ChildComponent />
+      Closing tag way <br/>
+      <ChildComponent></ChildComponent>
+      variable way <br/>
+      { ChildComponent() }
+    </div>
+  );
+};  
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent />);
